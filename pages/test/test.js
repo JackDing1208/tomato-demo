4cdcf4e9a9abe90c9103bd1f46dfdc6750a6f27c
JackDing <361164217@qq.com>
2019-05-14 17:24:58 +0800

    创建小程序

---
+++ pages/test/test.js
@@ -0,0 +1,45 @@
+// pages/test/test.js
+Page({
+
+  data: {
+    text:"haha"
+  },
+
+  onLoad: function (options) {
+
+  },
+
+
+  onReady: function () {
+
+  },
+
+
+  onShow: function () {
+
+  },
+
+
+  onHide: function () {
+
+  },
+
+
+  onUnload: function () {
+
+  },
+
+  onPullDownRefresh: function () {
+
+  },
+
+
+  onReachBottom: function () {
+
+  },
+
+
+  onShareAppMessage: function () {
+
+  }
+})
