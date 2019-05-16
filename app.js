eb0e4aef57069de60adafa20a7b6d34359009c93
JackDing <361164217@qq.com>
2019-05-16 21:45:20 +0800

    删除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',
-    app_id: "wx6acd76632421e206",
-    app_secret: "a443d4be5b9ea4914574aa9f8478cec0",
-    t_app_id: "hnf4awpyUV5J9y7WhA85gpUC",
-    t_app_secret: "zSZx8qB37pTsfoahnQBy5S6b"
-  }
-})
